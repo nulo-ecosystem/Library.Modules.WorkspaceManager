99dff55630c1536a2fa41cebd45da0bcd2835275
Kastney Frazão <kastney@outlook.com>
2023-12-18 22:10:54 -0300

    ♻️ refactor - Changing the name of the main form

--- DockPanelSuite/Source/DockPanelSuite.Forms/Form.Designer.cs
+++ DockPanelSuite/Source/DockPanelSuite.Forms/Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DockPanelSuite.Forms
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1058, 664);
+            Name = "Form";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form";
+            ResumeLayout(false);
         }
 
         #endregion
